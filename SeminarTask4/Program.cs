133568ff3f7e7163eb48243531a6c21d6fb9b344
invas1ve <pavellomako.info@gmail.com>
2022-07-26 15:50:11 +0300

    Added solution of SeminarTask4

---
+++ SeminarTask4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enter your number...");
+string number = Console.ReadLine();
+if (number.Length == 3)
+{
+    if (number == "000")
+    {
+        Console.WriteLine("Not a number");
+    }
+    else
+    {
+        int n = int.Parse(number);
+        int lastDigit = n % 10;
+        Console.WriteLine("The last digit is: " + lastDigit);
+    }
+
+}
+else
+{
+    Console.WriteLine("Please enter three-digit number and try again");
+}
