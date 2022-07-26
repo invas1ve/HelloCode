d8ed892fc6b1a471a341893d1f8a7ff3fc206d36
invas1ve <pavellomako.info@gmail.com>
2022-07-26 14:49:34 +0300

    Added solution of interval task

---
+++ Seminar1Interval/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter number...");
+string number = Console.ReadLine();
+int n = int.Parse(number);
+for (int i = -n; i <= n; i++)
+{ 
+  if(i == n)
+  {
+    Console.Write(i);
+  }
+  else
+  {
+    Console.Write(i + ", ");
+  }
+}
