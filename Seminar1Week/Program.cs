e6354f4f98ea3f45155c37afdaa2d6646d4c74d9
invas1ve <pavellomako.info@gmail.com>
2022-07-26 12:23:59 +0300

    Added week number task for seminar 1

---
+++ Seminar1Week/Program.cs
@@ -0,0 +1,38 @@
+﻿int monday = 1, tuesday = 2, 
+    wednesday = 3, thursday = 4, friday = 5, saturday = 6, sunday = 7;
+Console.WriteLine("Type your number");
+string console = Console.ReadLine();
+int con = int.Parse(console);
+if(con == monday)
+{
+ Console.WriteLine("It's monday");
+}
+else if(con == tuesday)
+{
+ Console.WriteLine("It's tuesday");
+}
+else if(con == wednesday)
+{
+ Console.WriteLine("It's wednesday");
+}
+else if(con == thursday)
+{
+ Console.WriteLine("It's thursday");
+}
+else if(con == friday)
+{
+ Console.WriteLine("It's friday");
+}
+else if(con == saturday)
+{
+ Console.WriteLine("It's saturday");
+}
+else if(con == sunday)
+{
+ Console.WriteLine("It's sunday");
+}
+else
+{
+ Console.WriteLine("There is no such thing! :)");
+}
+
