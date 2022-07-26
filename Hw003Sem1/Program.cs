222b56185af606d25a702581ef7e74e7b9db4cd6
invas1ve <pavellomako.info@gmail.com>
2022-07-26 16:34:47 +0300

    Добавили 3 задание к 1 семинару

---
+++ Hw003Sem1/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите число...");
+string a = Console.ReadLine();
+int number = Convert.ToInt32(a);
+if (number % 2 == 0)
+{
+    System.Console.WriteLine("Число четное");
+}
+else
+{
+    System.Console.WriteLine("Число нечетное");
+}
