e483c627b53db1ca677f57cf5476f6f5507b5513
invas1ve <pavellomako.info@gmail.com>
2022-07-26 11:51:49 +0300

    Added task 1 fom seminar 1

---
+++ Seminar1Square/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter first number: ");
+string number1 = Console.ReadLine();
+Console.WriteLine("Enter second number: ");
+string number2 = Console.ReadLine();
+int num1 = int.Parse(number1);
+int num2 = int.Parse(number2);
+int squareNum1 = num1 * num1;
+if(num2 == squareNum1)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
