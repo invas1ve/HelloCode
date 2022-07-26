e4b848b8514fa6d80235cbc28bb3a5740c5150de
invas1ve <pavellomako.info@gmail.com>
2022-07-26 16:15:08 +0300

    Homework 1 seminar 1st task

---
+++ Hw001Sem1/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 2, b = 10;
+if (a > b)
+{
+    Console.WriteLine("max = " + a);
+}
+else if (a < b)
+{
+    Console.WriteLine("max = " + b);
+}
+else
+{
+    Console.WriteLine("the numbers are equal");
+}
