12f811e8ee21b52bb7a6cb31002a641bc5e1741c
invas1ve <pavellomako.info@gmail.com>
2022-07-28 16:33:00 +0300

    added task3 sem2

---
+++ sem2_003/Program.cs
@@ -0,0 +1,13 @@
+﻿string str = Console.ReadLine();
+int number = int.Parse(str);
+int num1Check = 7;
+int num2Check = 23;
+
+if ((number % num1Check) == 0 && (number % num2Check == 0))
+{
+    System.Console.WriteLine("Число кратно 7 и 23");
+}
+else
+{
+    System.Console.WriteLine("Число не кратно 7 и 23");
+}
