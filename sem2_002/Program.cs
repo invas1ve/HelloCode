5fe76c4261d4de54a50d625508c4f345c97194e9
invas1ve <pavellomako.info@gmail.com>
2022-07-28 15:47:16 +0300

    Added task3 sem2

---
+++ sem2_002/Program.cs
@@ -0,0 +1,20 @@
+﻿System.Console.WriteLine("Введите первое число...");
+string number1 = Console.ReadLine();
+System.Console.WriteLine("Введите второе число...");
+string number2 = Console.ReadLine();
+
+int num1 = int.Parse(number1);
+int num2 = int.Parse(number2);
+
+if (num1 % num2 == 0)
+{
+    System.Console.WriteLine("Кратно");
+}
+else if (num1 % num2 != 0)
+{
+    System.Console.WriteLine($"Не кратно, остаток {num1 % num2}");
+}
+else
+{
+    System.Console.WriteLine("Проверьте что вы ввели числа");
+}
