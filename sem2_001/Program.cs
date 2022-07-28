2536fc01c8b608f44a80f344f10432e1d638c12f
invas1ve <pavellomako.info@gmail.com>
2022-07-28 14:44:25 +0300

    Задача 2 семинар 2

---
+++ sem2_001/Program.cs
@@ -0,0 +1,16 @@
+﻿int number = new Random().Next(10,100);
+System.Console.WriteLine("Случайное число: " + number);
+int number1 = number / 10;
+int number2 = number % 10;
+
+if (number1 > number2)
+{
+    System.Console.Write("Наибольшая цифра: ");
+    System.Console.WriteLine(number1);
+}
+else if (number2 > number1)
+{
+    System.Console.Write("Наибольшая цифра: ");
+    System.Console.WriteLine(number2);
+}
+else System.Console.WriteLine("Обе цифры одинаковы");
