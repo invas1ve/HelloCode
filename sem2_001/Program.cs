285e7ba9e4adb9cf6c7840e64652667a21c9c256
invas1ve <pavellomako.info@gmail.com>
2022-07-28 15:13:24 +0300

    updated program file

--- sem2_001/Program.cs
+++ sem2_001/Program.cs
@@ -1,16 +1,14 @@
 ﻿int number = new Random().Next(10,100);
-System.Console.WriteLine("Случайное число: " + number);
+System.Console.WriteLine($"Случайное число: {number}");
 int number1 = number / 10;
 int number2 = number % 10;
 
 if (number1 > number2)
 {
-    System.Console.Write("Наибольшая цифра: ");
-    System.Console.WriteLine(number1);
+    System.Console.WriteLine($"Наибольшая цифра: {number1} ");
 }
 else if (number2 > number1)
 {
-    System.Console.Write("Наибольшая цифра: ");
-    System.Console.WriteLine(number2);
+    System.Console.WriteLine($"Наибольшая цифра: {number2}");
 }
 else System.Console.WriteLine("Обе цифры одинаковы");
