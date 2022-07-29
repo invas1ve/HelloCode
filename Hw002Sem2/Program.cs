d750934d382eeb5277e52bc3d3a634a7d76927a8
invas1ve <pavellomako.info@gmail.com>
2022-07-29 10:30:12 +0300

    Сделал задачу №2

---
+++ Hw002Sem2/Program.cs
@@ -0,0 +1,7 @@
+﻿int num = new Random().Next(100, 1000);
+System.Console.WriteLine($"Случайное трехзначное число - {num}");
+int num1 = num / 100;
+int num3 = num % 10;
+string result = num1.ToString() + num3.ToString();
+int res = Convert.ToInt32(result);
+System.Console.WriteLine($"Результат = {res}");
