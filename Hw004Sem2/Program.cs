719dccbb8b386aa7c21d37fca68b78038fd49699
invas1ve <pavellomako.info@gmail.com>
2022-07-29 11:36:32 +0300

    Задание 4 семинар 2

---
+++ Hw004Sem2/Program.cs
@@ -0,0 +1,18 @@
+﻿System.Console.WriteLine("Введите цифру...");
+int n = int.Parse(Console.ReadLine());
+if (n >= 1 && n <= 7)
+{
+    if (n >= 1 && n <= 5)
+    {
+        System.Console.WriteLine("Будний день");
+    }
+    else if (n == 6 || n == 7)
+    {
+        System.Console.WriteLine("Выходной");
+    }
+}
+else 
+{
+    System.Console.WriteLine("Введите цифру обозначающую день недели от 1 до 7");
+}
+
