b32f58f4cb1392268032beb11dc239244fbe9039
invas1ve <pavellomako.info@gmail.com>
2022-07-29 11:15:19 +0300

    third digit of number

---
+++ Hw003Sem2/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите число... ");
+int n = int.Parse(Console.ReadLine());
+if (n > 99)
+{
+   string number = n.ToString();
+   System.Console.WriteLine($"Третья цифра - {number[2]}");
+}
+else
+{
+    System.Console.WriteLine($"Число - {n}, третьей цифры нет");
+}
